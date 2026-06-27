6eef223c7aeb04a951fbd552a3415664dc5e714f
BluiWulf <bluiwulf@gmail.com>
2026-06-27 15:21:43 -0400

    Created normalize_url() function

--- crawl.py
+++ crawl.py
@@ -1,3 +1,10 @@
 
-def normalize_url():
-    pass
+from urllib.parse import urlparse
+
+def normalize_url(input_url: str) -> str:
+    url_Parts = tuple(urlparse(input_url))
+    normal_url = ""
+    for part in url_Parts[1:]:
+        normal_url += part
+
+    return normal_url.lower().rstrip("/")
--- test_crawl.py
+++ test_crawl.py
@@ -3,4 +3,11 @@ import unittest
 from crawl import normalize_url
 
 class TestCrawl(unittest.TestCase):
-    pass
+    def test_normalize_url(self):
+        input_url = "https://www.boot.dev/blog/path"
+        actual = normalize_url(input_url)
+        expected = "www.boot.dev/blog/path"
+        self.assertEqual(actual, expected)
+
+if __name__ == "__main__":
+    unittest.main()
